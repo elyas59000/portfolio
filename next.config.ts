2b15f97f25c6d8493c0bb1c31ba27eb710baeac9
elyas rabhiu <elyas.rabhiu.etu@univ-lille.fr>
2026-01-04 13:33:02 +0100

    initailaisation

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export',      // Génère un site statique (HTML) pour GitLab
+  images: {
+    unoptimized: true,   // Obligatoire pour les images sur GitLab Pages
+  },
 };
 
 export default nextConfig;
