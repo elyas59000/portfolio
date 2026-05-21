dad01f2124e90659ce0d422d1cfb3827ab7597ad
Elyas <elyas.rabhiu@gmail.com>
2026-05-21 21:57:17 +0700

    fix url

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',  
-  basePath: '/elyas-rabhiu',    
+  basePath: '/portfolio-43c6d4',    
   images: {
     unoptimized: true,  
   },
