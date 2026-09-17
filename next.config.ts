d4d0f04c20b99a0600ee1ccd7f5590e0971c0d47
elyas rabhiu <elyas.rabhiu.etu@univ-lille.fr>
2026-09-17 20:03:09 +0200

    ouiii

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',  
-  basePath: '/portfolio-43c6d4',    
+  basePath: '/portfolio',    
   images: {
     unoptimized: true,  
   },
